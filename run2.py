706daa10f782f07e4452947c4f3eff458d5a1175
ranjeeth1201 <52576113+ranjeeth1201@users.noreply.github.com>
2019-07-15 09:49:18 +0530

    Update run2.py

--- run2.py
+++ run2.py
@@ -1,8 +1,8 @@
 zz=input()
 if((zz>'a' and zz<='z')or(zz>='A' and zz<='Z')):
-if(zz=='a' or zz=='e' or zz=='i' or zz=='o' or zz=='u' or zz=='A' or zz=='E' or zz=='I' or zz=='O' or zz=='U')
-print('Vowel')
-else:
-print('Consonant')
-else:
-print('Invalid')
+  if(zz=='a' or zz=='e' or zz=='i' or zz=='o' or zz=='u' or zz=='A' or zz=='E' or zz=='I' or zz=='O' or zz=='U')
+  print('Vowel')
+  else:
+    print('Consonant')
+    else:
+      print('Invalid')
